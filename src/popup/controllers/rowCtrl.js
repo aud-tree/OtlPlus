9b13fc72c70b2151a63b02df32e916120361296c
Audrey Schwarz <acbschwarz@gmail.com>
2014-04-01 17:52:40 -0500

    Autocomplete T-F hours after entering M hours for the first time

--- src/popup/controllers/rowCtrl.js
+++ src/popup/controllers/rowCtrl.js
@@ -6,7 +6,19 @@ angular.module('OtlPlusControllers')
       }, 0);
     };
 
-    $scope.autocomplete = function(index) {
+    if(undefined === $scope.row.autocompleteEnabled) {
+      $scope.row.autocompleteEnabled = true;
+    }
+    $scope.row.cachedMondayHours = $scope.row.cachedMondayHours || 0;
 
+    $scope.autocomplete = function(index) {
+      if(index == 1 && $scope.row.autocompleteEnabled && $scope.row.hours[1] != $scope.row.cachedMondayHours) {
+        [2,3,4,5].forEach(function(index, i, arr) {
+          $scope.row.hours[index] = $scope.row.hours[1];
+        });
+        $scope.row.autocompleteEnabled = false;
+        $scope.row.cachedMondayHours = $scope.row.hours[1];
+        $scope.cache();
+      }
     };
   }]);
